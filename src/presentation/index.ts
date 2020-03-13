59303eeba5d3a1468a32c0fc1e1073989658793c
Yan Sabino <yan.sabino@hotmail.com>
2020-03-13 12:56:59 -0300

    getMovieById up and running

--- src/presentation/index.ts
+++ src/presentation/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
 import { createMovieEndpoint } from "./endpoints/movies/createMovie"
+import { getMovieByIdEndPoint } from "./endpoints/movies/getMovieById"
+
 
 const app = express();
 app.use(express.json());
 
 app.post("/movie", createMovieEndpoint)
+app.get("/movie/:id", getMovieByIdEndPoint)
 
 export default app;
